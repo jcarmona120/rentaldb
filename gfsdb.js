ad94bbed0ba0f841120022e513f1b5a4cbb172d8
Jawann Carmona <carmona.jawann@gmail.com>
2018-06-28 23:25:18 -0400

    Update gfsdb.js

--- gfsdb.js
+++ gfsdb.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var db = mongoose.connection;
 
 mongoose.connect(
-  "mongodb://gfsdb138:gfsdb138@ds263500.mlab.com:63500/gfsdatabase"
+  ENTER KEY HERE
 );
 
 db.on("error", console.error.bind(console, "connection error"));
